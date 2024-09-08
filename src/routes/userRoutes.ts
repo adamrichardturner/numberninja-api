d9e082595d9156310211071672472a2a8376f990
adamrichardturner <hello@adamrichardturner.dev>
2024-09-08 13:32:42 +0100

    Update delete method

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,6 @@ import { firebaseAuth } from "../middleware/firebaseAuth";
 
 const router = express.Router();
 
-router.delete("/delete-account", firebaseAuth, deleteUserData);
+router.delete("/user/delete-account", firebaseAuth, deleteUserData);
 
 export default router;
