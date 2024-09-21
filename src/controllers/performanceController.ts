da743d73d62812bf9bc04f7672b72269648b14c0
adamrichardturner <hello@adamrichardturner.dev>
2024-09-21 21:03:37 +0100

    Handle stats

--- src/controllers/performanceController.ts
+++ src/controllers/performanceController.ts
@@ -43,33 +43,6 @@ export const getOperationPerformance = async (req: Request, res: Response) => {
     }
 };
 
-export const getStrugglingQuestions = async (req: Request, res: Response) => {
-    try {
-        const firebaseUid = req.user?.uid;
-        if (!firebaseUid) {
-            return res.status(401).json({ error: "Unauthorized" });
-        }
-
-        const userId = uuidv5(firebaseUid, UUID_NAMESPACE);
-        const { operation } = req.query;
-
-        if (!operation) {
-            return res.status(400).json({ error: "Operation is required" });
-        }
-
-        const strugglingQuestions =
-            await performanceService.getStrugglingQuestions(
-                userId,
-                operation as string,
-            );
-
-        res.json(strugglingQuestions);
-    } catch (error) {
-        console.error("Error fetching struggling questions:", error);
-        res.status(500).json({ error: "Failed to fetch struggling questions" });
-    }
-};
-
 export const getTotalStats = async (req: Request, res: Response) => {
     try {
         const firebaseUid = req.user?.uid;
@@ -88,27 +61,6 @@ export const getTotalStats = async (req: Request, res: Response) => {
     }
 };
 
-export const getPerformanceOverTime = async (req: Request, res: Response) => {
-    try {
-        const firebaseUid = req.user?.uid;
-        if (!firebaseUid) {
-            return res.status(401).json({ error: "Unauthorized" });
-        }
-
-        const userId = uuidv5(firebaseUid, UUID_NAMESPACE);
-
-        const performanceOverTime =
-            await performanceService.getPerformanceOverTime(userId);
-
-        res.json(performanceOverTime);
-    } catch (error) {
-        console.error("Error fetching performance over time:", error);
-        res.status(500).json({
-            error: "Failed to fetch performance over time",
-        });
-    }
-};
-
 export const getCommonWrongAnswers = async (req: Request, res: Response) => {
     try {
         const firebaseUid = req.user?.uid;
@@ -122,7 +74,7 @@ export const getCommonWrongAnswers = async (req: Request, res: Response) => {
             : 20;
 
         const commonWrongAnswers =
-            await performanceService.getCommonWrongAnswers(userId, limit);
+            await performanceService.getCommonWrongAnswers(userId);
 
         res.json(commonWrongAnswers);
     } catch (error) {
